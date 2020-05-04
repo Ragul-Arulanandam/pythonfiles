b37334057862c7561c193315e6196f1eb7b25ad6
sivaragul <srsivaragul@gmail.com>
2020-05-04 21:43:39 +0530

    second commit

--- badversion.py
+++ badversion.py
@@ -1,26 +1,26 @@
 import random
 
-n=int(input())
+'''n=int(input())
 def isbadversion(n):
     Number = random.randint(0,n)   
     return Number     
 
-print(isbadversion(n))
+print(isbadversion(n))'''
 
 def badversion(n):
-    l=0
-    r=n-1
-    min = 0
-    while l<=r:
-        mid = (l+r)//2
-        get = isbadversion(n)
+    start=0
+    end=n-1
+    get = random.randint(0,n)
+    print("the fail",get)
+    while start<=end:
+        mid = (start+end)//2
         if get == mid:
             return mid #"good versions"
         elif get>mid:
-            l=mid+1
+            start=mid+1
         else:
-            l=mid-1
+            start=mid-1
 
     return mid
 
-print(badversion(n))
+print(badversion(20))
