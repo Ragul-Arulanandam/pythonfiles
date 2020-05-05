0a2f5ca2a807be7c8a6d53221aafa75c6f34aa72
sivaragul <srsivaragul@gmail.com>
2020-05-05 22:53:00 +0530

    Third commit by ragul

--- badversion1.py
+++ badversion1.py
@@ -1,27 +1,23 @@
 import random
 
 n=int(input())    
-get = random.randint(0,n) #5
+get = random.randint(0,n) 
+print(n,get)
 
 def isbadversion(value):
-    if value == get:
+    if value >= get:
         return True
     else:
         return False
 def badversion(start ,end=n):
-    
-    mid = ((start+end)//2) #5
 
-    while start<=end:
-        if get<=0:
-            return "No good version is available"
+    mid = ((start+end)//2) 
+    if isbadversion(mid)== True:
+        if isbadversion(mid-1) == False:
+            return mid
         else:
-            if isbadversion(mid)== True:
-                if isbadversion(mid-1) == False:
-                    return mid
-                else:
-                    return badversion(0,mid)
-            else:
-                return badversion(mid,n)
+            return badversion(0,mid)
+    else:
+        return badversion(mid,n)
 
-print("The badversion is ",badversion(0,n))
+print(badversion(0,n))
